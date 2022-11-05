a33a8c2ffdc2ef3745768394f3243a6a992b85f6
Fabian Machicao <fabian.machicao@gmail.com>
2022-11-05 19:43:48 -0400

    Creación de barra de navegación entre fragments

--- app/src/main/java/com/andremachicao/ludoteca/MainActivity.kt
+++ app/src/main/java/com/andremachicao/ludoteca/MainActivity.kt
@@ -2,10 +2,39 @@ package com.andremachicao.ludoteca
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.andremachicao.ludoteca.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(JuegosFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.juegos -> replaceFragment(JuegosFragment())
+                R.id.chat -> replaceFragment(ChatFragment())
+                R.id.contacto -> replaceFragment(ContactFragment())
+
+                else -> {
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragManager = supportFragmentManager
+        val fragmentTransaction = fragManager.beginTransaction()
+        fragmentTransaction.replace(R.id.MainScreenFrameLayout,fragment)
+        fragmentTransaction.commit()
     }
 }
